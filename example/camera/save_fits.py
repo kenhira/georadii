bb56bf44945c4cbe549ab8323ea24fe3de04219e
Ken Hirata <ken.hirata@colorado.edu>
2025-03-06 15:02:28 -0700

    Minor mod for scaling factor option

--- example/camera/save_fits.py
+++ example/camera/save_fits.py
@@ -1,3 +1,20 @@
+"""
+save_fits.py
+
+author: Ken Hirata (@kenhira)
+
+This script reads a .fits file, processes the image data, and saves the resulting image as a .png file.
+
+Usage:
+	python3 save_fits.py <input_fits_file> <output_png_file> [scaling_factor]
+
+Arguments:
+	<input_fits_file> : str : Path to the input .fits file.
+	<output_png_file> : str : Name of the output .png file.
+	[scaling_factor] : float, optional : Factor to scale the image data. Default is 1.0.
+
+"""
+
 import sys
 import os
 import numpy as np
@@ -6,11 +23,14 @@ import matplotlib.pyplot as plt
 from georadii.util import read_fits
 
 if __name__ == "__main__":
-	factor = 25.
-	if len(sys.argv) == 3:
+	if len(sys.argv) in [3, 4]: # Check if the correct number of arguments were passed
 		print(f"Arguments received: {sys.argv[1:]}")
 		filename = sys.argv[1]
 		savename = sys.argv[2]
+		if len(sys.argv) == 4:
+			factor = float(sys.argv[3])
+		else:
+			factor = 1.0
 
 		# Extract the image data from .fits file
 		img, header  = read_fits(filename)
@@ -29,4 +49,4 @@ if __name__ == "__main__":
 		plt.savefig(fn_out, dpi=300)
 		plt.show()
 	else:
-		print("Not enough arguments were passed.")	
+		print("Two or three arguments need to be present.")	
